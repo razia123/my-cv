1d4e592b5f2dde0a3bc48f13703f555276a5ca47
Razia Haque <raziahaque1990@gmail.com>
2024-02-08 14:12:03 +0600

    my cv with html table and vue js

---
+++ src/components/MainContent.vue
@@ -0,0 +1,307 @@
+<template>
+    <!-- Main table start -->
+    <table align="center" width="65%">
+        <!-- image and my contacts -->
+        <tr align="left">
+            <td width="25%">
+                <img src="../assets/images/photo.jpg" alt="" height="280px" width="80%">
+            </td>
+            <td>
+                <h1>Razia Haque</h1>
+                <p>Address:KHA-19/1/3,Beparipara, Khilkhet, Dhaka</p>
+                <p>Email:raziahaque90@gmail.com</p>
+                <p>Mobile No:01301060788</p>
+                <p>Skype:razia.haque10</p>
+
+            </td>
+        </tr><br>
+        <!-- Objective-->
+        <tr align="left">
+            <td colspan="2">
+                <table>
+                    <tr>
+                        <h2><u>Career Objective</u></h2>
+                        <p>To pursue any suitable position in your organization. My career objective is to create good
+                            interpersonal skills through the organization. </p>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- academic info -->
+        <tr align="left">
+            <td colspan="2">
+                <h2><u>Academic Information</u></h2>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <table border="1px" width="100%">
+
+                    <tr align="center">
+                        <th>Exam Title</th>
+                        <th>Concentration/Major</th>
+                        <th>Institute</th>
+                        <th>Result</th>
+                        <th>Pas.Year</th>
+                        <th>Duration</th>
+
+                    </tr>
+                    <tr align="center">
+                        <td>Bachelor of Science (BSc)</td>
+                        <td>Computer Science & Engineering </td>
+                        <td>Ahsanullah University of Science and Technology </td>
+                        <td>CGPA:2.93 out of 4 </td>
+                        <td>2013 </td>
+                        <td>4</td>
+                    </tr>
+                    <tr align="center">
+                        <td>HSC</td>
+                        <td>Science </td>
+                        <td>Adamjee Cantonment College </td>
+                        <td>CGPA:4.8 out of 5 </td>
+                        <td>2007</td>
+                        <td>2</td>
+                    </tr>
+                    <tr align="center">
+                        <td>SSC </td>
+                        <td>Science</td>
+                        <td>Kurmitola High School And College </td>
+                        <td>CGPA:5 out of 5 </td>
+                        <td>2005 </td>
+                        <td>_</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- technical skill -->
+        <tr align="left">
+            <td colspan="2">
+                <table width="100%">
+                    <tr>
+                        <td colspan="3">
+                            <h2><u>Technical Skill</u></h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="30%">Programming Language</td>
+                        <td>:</td>
+                        <td>C,C++,C#,PHP</td>
+                    </tr>
+                    <tr>
+                        <td>Framework</td>
+                        <td>:</td>
+                        <td>ASP.NET</td>
+                    </tr>
+                    <tr>
+                        <td>Database</td>
+                        <td>:</td>
+                        <td>MySQL</td>
+                    </tr>
+                    <tr>
+                        <td>Scripting Language</td>
+                        <td>:</td>
+                        <td>JavaScript</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- communication skills -->
+        <tr align="left">
+            <td colspan="2">
+                <h2><u>Communication Skills:</u></h2>
+                <p>Fluent in Bengali and English – both verbal and written.</p>
+
+            </td>
+        </tr>
+
+        <!-- professional training -->
+        <tr align="left">
+            <td colspan="2">
+                <table width="100%">
+                    <tr>
+                        <td colspan="2">
+                            <h2><u>Professional Training</u></h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td  width="5%" valign="top">
+                            <h4>1.</h4>
+                        </td>
+                        <td>
+                            <p>
+                                OBJECT ORIENTED PROGRAMMING & C#(ASP.NET)
+                            </p>
+                            <p>
+                                DURATION:11 weeks
+                            </p>
+                            <p>BASIS,Dhaka</p>
+                        </td>
+                       
+                    </tr>
+                    <tr>
+                        <td  width="5%" valign="top">
+                            <h4>2.</h4>
+                        </td>
+                        <td>
+                            <p>
+                                PHP with Laravel (SEIP)
+                            </p>
+                            <p>
+                                DURATION:3 months
+                            </p>
+                            <p>BASIS,Dhaka</p>
+                        </td>
+                       
+                    </tr>
+                </table>
+
+            </td>
+        </tr>
+        <!-- Employment History -->
+        <tr align="left">
+            <td colspan="2">
+                <table width="100%">
+                    <tr>
+                        <td colspan="2">
+                            <h2><u>Employment History</u></h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="5%" valign=top>
+                            <h4>1.</h4>
+                        </td>
+                        <td>
+                            <h4>
+                                <u>
+                                    Jr. Software Engineer (0.3 yr)
+                                </u>
+                            </h4>
+                            <p>Bangla Drop Shipping Ltd.<br>
+                                Area of Expertise: PHP (Laravel) (0.3 yr)<br>
+                                (1 Nov 2023 - Continuing)</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="5%" valign=top>
+                            <h4>2.</h4>
+                        </td>
+                        <td>
+                            <h4>
+                                <u>
+                                    Internship (0.3 yr)
+                                </u>
+                            </h4>
+                            <p>Nagadhat Bangladesh Limited<br>
+                                Area of Expertise: PHP (Laravel) (0.3 yr)<br>
+                                (1 Jul 2023 - 31 Oct 2023) </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="5%" valign=top>
+                            <h4>3.</h4>
+                        </td>
+                        <td>
+                            <h4>
+                                <u>
+                                    Software Developer (1.3 yrs)
+                                </u>
+                            </h4>
+                            <p>The Databiz Software Company Ltd<br>
+                                Area of Expertise: C Programming (0.6 yr), MySQL database (0.6 yr)<br>
+                                (1 Oct 2013 - 1 Jan 2015) </p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- personal info -->
+        <tr align="left">
+            <td colspan="2">
+                <table width="100%">
+                    <tr height="50px">
+                        <td colspan="3">
+                            <h2><u>Personal Information</u></h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Father Name</td>
+                        <td>:</td>
+                        <td>Md. Ezabul Haqe</td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Mother Name</td>
+                        <td>:</td>
+                        <td>Tahera Begum</td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Date of Birth</td>
+                        <td>:</td>
+                        <td>10<sup>th</sup> October 1990</td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Gender</td>
+                        <td>:</td>
+                        <td>Female</td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Religion</td>
+                        <td>:</td>
+                        <td>Islam</td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Marital Status</td>
+                        <td>:</td>
+                        <td>Single</td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Blood Group</td>
+                        <td>:</td>
+                        <td>B+</td>
+                    </tr>
+                    <tr>
+                        <td width="25%">District</td>
+                        <td>:</td>
+                        <td>Chapai Nawabganj</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- Certification -->
+        <tr align="left">
+            <td colspan="2">
+                <table width="100%">
+                    <tr>
+                        <td>
+                            <h2><u>Certification</u></h2>
+                            <p>I certified that to the best of khowladge and belief, these data correctly described my
+                                qualification and me.</p>
+                        </td>
+                    </tr>
+
+                </table>
+            </td>
+        </tr><br>
+
+        <!-- signature -->
+        <tr align="right">
+            <td colspan="2">
+                <img src="../assets/images/logo.jpg" alt="" width="20%" height="40px">
+                <h3>Signature</h3>
+            </td>
+        </tr>
+    </table>
+    <!-- Main table end -->
+</template>
+<script>
+export default {
+  name: 'MainContent'
+}
+</script>
+<style scoped>
+
+</style>
